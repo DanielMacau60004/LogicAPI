07081dad1a4cb2071c0721598ca8b56ff8917058
DanielMacau <d.macau@campus.fct.unl.pt>
2025-03-08 12:28:03 +0000

    Documentation files

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.logic.api","l":"IFOLExp"},{"p":"com.logic.api","l":"IPLExp"},{"p":"com.logic.api","l":"LogicAPI"}];updateSearchResults();
